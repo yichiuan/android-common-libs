c01063c3ae4970774acb032ee9628ede921e2e2a
yichiuan <yang.yichiuan@gmail.com>
2017-05-26 19:18:46 +0800

    Photos can get orientation from  InputStream

--- library/src/main/java/com/yichiuan/common/Photos.java
+++ library/src/main/java/com/yichiuan/common/Photos.java
@@ -1,8 +1,9 @@
 package com.yichiuan.common;
 
-import android.media.ExifInterface;
+import android.support.media.ExifInterface;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public class Photos {
     public static int getOrientationFromExif(String filename) throws IOException {
@@ -11,16 +12,29 @@ public class Photos {
         int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION,
                 ExifInterface.ORIENTATION_NORMAL);
 
+        return getDegree(orientation);
+    }
+
+    public static int getOrientationFromExif(InputStream is) throws IOException {
+
+        ExifInterface exif = new ExifInterface(is);
+        int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                ExifInterface.ORIENTATION_NORMAL);
+
+        return getDegree(orientation);
+    }
+
+    public static int getDegree(int orientation) {
         int degree;
         switch (orientation) {
-            case ExifInterface.ORIENTATION_ROTATE_270:
-                degree = 270;
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                degree = 90;
                 break;
             case ExifInterface.ORIENTATION_ROTATE_180:
                 degree = 180;
                 break;
-            case ExifInterface.ORIENTATION_ROTATE_90:
-                degree = 90;
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                degree = 270;
                 break;
             default:
                 degree = 0;
